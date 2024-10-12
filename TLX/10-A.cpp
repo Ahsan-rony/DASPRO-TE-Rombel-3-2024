91e8da293dc7524476ac6f6c8afd75b7b57b9abc
Khasanudin02 <muhammadkhasanudin02@gmail.com>
2024-10-12 12:35:25 +0700

    Create 10-A.cpp
    penginstalan kompilator c++

---
+++ TLX/10-A.cpp
@@ -0,0 +1,16 @@
+/* Untuk memastikan bahwa Anda telah berhasil menginstal g++ dengan benar, bukalah sebuah terminal/shell, lalu ketikkan perintah berikut:
+g++ --version
+Tekan Enter. Perintah tersebut akan mengeluarkan sejumlah baris yang mirip seperti berikut:
+
+g++ …
+Copyright (C) …
+This is free software; …
+Pastikan Anda mendapat keluaran seperti di atas!
+
+Lalu, copy dan paste-lah keseluruhan baris pertama dari keluaran yang tercetak pada terminal/shell Anda (yakni, baris yang diawali dengan g++ dan berisi informasi versi kompilator) sebagai jawaban dari soal ini*/
+
+jika muncul seperti ini pada command prompt
+
+g++ (x86_64-posix-seh-rev0, Built by MinGW-W64 project) 8.1.0
+
+itu berarti penginstalan minGW atau compilator c++ sudah berhasil
