f953b51b1221e3741a69790f921806559d37d789
gavrielariel <gavrielariel325@gmail.com>
2024-10-12 23:25:46 +0700

    Update 5-A.cpp

--- TLX/Chapter 5/5-A.cpp
+++ TLX/Chapter 5/5-A.cpp
@@ -1,9 +1,5 @@
-/* Pak Dengklek memiliki kandang-kandang persegi, dengan panjang sisinya adalah setiap bilangan bulat antara 121 sampai dengan 125 (inklusif). Ia ingin mengetahui luas setiap kandangnya. Dengan perulangan while, kodenya adalah:*/
-// int sisi = 121;
-// while (sisi <= 125) {
-//    cout << sisi * sisi << endl;
-//    sisi += 1;
-//}
+// Pak Dengklek memiliki kandang-kandang persegi, dengan panjang sisinya adalah setiap bilangan bulat antara 121 sampai dengan 125 (inklusif). Ia ingin mengetahui luas setiap kandangnya. //
+
 #include <iostream> // Menyertakan library input-output standar C++
 using namespace std; // Memungkinkan penggunaan elemen dari namespace std tanpa prefiks 'std::'
 
@@ -12,9 +8,9 @@ int main() { // Fungsi utama tempat eksekusi program dimulai
         cout << sisi * sisi << endl; // Menghitung kuadrat dari 'sisi' dan mencetaknya
     }
 }
-// keluaran yang tercetak adalah 
-//14641
-//14884
-//15129
-//15376
-//15625
+/* keluaran yang tercetak adalah 
+   14641
+   14884
+   15129
+   15376
+   15625 */
