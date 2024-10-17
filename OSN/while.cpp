e80c9b0da2dc1dff641f20dfa717bfe37dae0f4c
Ahsan-rony <ahsanronykusuma@students.unnes.ac.id>
2024-10-18 00:33:02 +0700

    Update while.cpp

--- OSN/while.cpp
+++ OSN/while.cpp
@@ -1,12 +1,12 @@
 #include <cstdio>
 
-  int main() {
+int main() {
   int N;
   scanf("%d", &N);
-    
+  
   int i = 0;
   while (i < N) {
-    printf("tulisan ini dicetak saat i = %d\n", i);
+  printf("tulisan ini dicetak saat i = %d\n", i);
     i++;
   }
   printf("akhir dari program\n");
