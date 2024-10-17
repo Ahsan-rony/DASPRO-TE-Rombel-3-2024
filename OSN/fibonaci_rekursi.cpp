f699e152d1ac996966f8a07a659faea608fb4526
Rhasyaptr <rhasyasera@gmail.com>
2024-10-18 05:44:41 +0700

    fibonaci_rekursi.cpp

--- OSN/fibonaci_rekursi.cpp
+++ OSN/fibonaci_rekursi.cpp
@@ -7,12 +7,3 @@ int fibonacci(int N) {
         return fibonacci(N - 1) + fibonacci(N - 2);
     }
 }
-
-int main() {
-    int N = 4; // Mengganti N dengan 4
-
-    // Menghitung dan mencetak angka Fibonacci
-    printf("Fibonacci dari %d adalah %d\n", N, fibonacci(N));
-
-    return 0;
-}
