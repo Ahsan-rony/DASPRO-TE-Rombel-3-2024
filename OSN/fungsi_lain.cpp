432b5fee042081307050821f957d67f6efb219bf
DestaNurdiansyah <destanurdiansyah@students.unnes.ac.id>
2024-10-17 22:58:18 +0700

    Update fungsi_lain.cpp

--- OSN/fungsi_lain.cpp
+++ OSN/fungsi_lain.cpp
@@ -1,14 +1,13 @@
-#include <iostream>
-using namespace std;
+#include<cstdio>
 
-int main(int x) {
-    if (x < 2) {
-        return false;
-    }
-    for (int i = 2; i*i <= x; i++) {
-        if (x % i == 0) {
-            return false;
-        }
-    }
-    return true;
+bool prima(int x) {
+if (x < 2) {
+return false;
+}
+for (int i = 2; i*i <= x; i++) {
+if (x % i == 0) {
+return false;
+}
+}
+return true;
 }
