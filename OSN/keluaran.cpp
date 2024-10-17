545de3c235af771172d03df5b3283ea058683193
Eqyura <agha.hrabbani@gmail.com>
2024-10-17 21:02:13 +0700

    Create keluaran.cpp

---
+++ OSN/keluaran.cpp
@@ -0,0 +1,10 @@
+
+#include <cstdio>
+  int main() {
+     int a, b; 
+     printf("masukkan nilai a: "); 
+     scanf("%d", &a);
+     printf("masukkan nilai b: "); 
+     scanf("%d", &b); 
+     printf("hasil dari penjumlahan a dan b: %d\n", a+b); 
+}
