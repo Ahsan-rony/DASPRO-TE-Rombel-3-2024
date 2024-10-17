d9bfcc5665d626f45ec90b20280b2aaa1034d991
Ignasius Adiwidya Aryanta <ignasiusra@gmail.com>
2024-10-17 21:46:49 +0700

    Update cek.txt

--- TLX/Chapter 4/cek.cpp
+++ TLX/Chapter 4/cek.cpp
@@ -1,4 +1,36 @@
-Pilihan Ganda:
-- [/] Pilihan 1
-- [ ] Pilihan 2
-- [ ] Pilihan 3
+3.
+Apakah keluaran dari program berikut?
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+
+    if (x > 10) {
+        cout << "nilai x lebih dari 10" << endl;
+    }
+    if (x <= 10) {
+        cout << "nilai x kurang dari atau sama dengan 10" << endl;
+    }
+    if (x >= 9) {
+        cout << "nilai x lebih dari atau sama dengan 9" << endl;
+    }
+}
+
+a
+tidak ada keluaran yang dihasilkan
+
+b
+nilai x kurang dari atau sama dengan 10
+
+c
+nilai x kurang dari atau sama dengan 10
+nilai x lebih dari atau sama dengan 9
+
+d
+nilai x lebih dari atau sama dengan 9
+
+e
+nilai x lebih dari 10
+nilai x lebih dari atau sama dengan 9
