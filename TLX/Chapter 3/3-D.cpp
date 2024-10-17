a8b5ed1a602769fe67a3e5499ad738a578d5b7be
abimanyu-rma <ributabimanyu.abi@gmail.com>
2024-10-17 20:36:44 +0700

    Create 3-D.cpp

---
+++ TLX/Chapter 3/3-D.cpp
@@ -0,0 +1,59 @@
+/*
+1.Berapa kalikah C++ akan tercetak?
+*/
+
+int counter = 0; //perintah awalan dimulai dari 0
+ while (counter < 4) { //perintah kurang dari 4 
+    cout << "C++" << endl; //mencetak c++
+    counter += 1; //selalu ditambah 1 
+}
+/*
+a.3
+b.(4)
+c.5
+d.6
+*/
+/* 
+2.Berapa kalikah C++ akan tercetak?
+*/
+int counter = 1; // perindah awalan dimulai dari 1
+while (counter < 4) { // perintah kurang dari 4
+    cout << "C++" << endl; // mencetak c++
+    counter += 1; //selalu ditambah 1
+}
+/*
+a.(3)
+b.4
+c.5
+d.6
+*/
+/*
+3.Berapa kalikah C++ akan tercetak?
+*/
+int counter = 1;  // perindah awalan dimulai dari 1
+while (counter <= 4) { // perintah kurang dari sama dengan 4
+    cout << "C++" << endl; // mencetak c++
+    counter += 1; //selalu ditambah 1
+}
+//Operator <= bermakna "kurang dari atau sama dengan"
+/*
+a.3
+b.4
+c.5
+d.6
+*/
+/*
+4.Berapa kalikah C++ akan tercetak?
+*/
+int counter = 1;// perindah awalan dimulai dari 1
+while (counter <= 6) { //perintah kurang dari sama dengan 6
+    cout << "C++" << endl;// mencetak c++
+    counter += 2;// selalu ditambah 2
+}
+/*
+a.3
+b.4
+c.5
+d.6
+*/
+
