429b16d0c9894d3974f11b36cf59cbd9f310529f
nandifa27 <pentolodon27@gmail.com>
2024-10-17 22:20:44 +0700

    Add files via upload

---
+++ TLX/Chapter 1/1-F.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    // cetak luas kandang
+    std::cout << 364 * 79 << std::endl;
+
+    // cetak keliling kandang
+    std::cout << 2 * (364 + 79) << std::endl;
+}
+
