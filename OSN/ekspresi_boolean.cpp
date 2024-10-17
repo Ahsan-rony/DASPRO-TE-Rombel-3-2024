267254914b5ebb73b82f674f3dfd161b1ade029f
gg3106 <138283387+gg3106@users.noreply.github.com>
2024-10-18 01:03:40 +0700

    Create ekspresi_boolean.cpp

---
+++ OSN/ekspresi_boolean.cpp
@@ -0,0 +1,7 @@
+if ((x > 0) && (x % 2 == 1)) {
+printf("positif dan ganjil\n");
+} else if ((x > 0) && (x % 2 == 0)) {
+printf("positif dan genap\n");
+} else if ((x < 0) && (x % 2 == 1)) {
+printf("negatif dan ganjil\n");
+} else if ((x < 0) && (x % 2 == 0)) {
