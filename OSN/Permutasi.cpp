c99133e1b7723459f9a9eee81eea4fa66a116ef6
zhafranbilly <billyemailnyadua@gmail.com>
2024-10-18 08:12:00 +0700

    Create Permutasi.cpp

---
+++ OSN/Permutasi.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>  // Untuk fungsi printf
+
+int main() {
+    // Loop tiga kali untuk setiap variabel
+    for (int i = 1; i <= 3; i++) {
+        for (int j = 1; j <= 3; j++) {
+            for (int k = 1; k <= 3; k++) {
+                printf("%d%d%d\n", i, j, k);  // Output i, j, k
+            }
+        }
+    }
+    
+    return 0;
+}
