4b1c69a1460514e68d7b0b704b19bd37dc1858e4
gg3106 <138283387+gg3106@users.noreply.github.com>
2024-10-20 00:10:02 +0700

    Update ekspresi_boolean.cpp

--- OSN/06. Percabangan/ekspresi_boolean.cpp
+++ OSN/06. Percabangan/ekspresi_boolean.cpp
@@ -1,7 +1,23 @@
-if ((x > 0) && (x % 2 == 1)) {
-printf("positif dan ganjil\n");
-} else if ((x > 0) && (x % 2 == 0)) {
-printf("positif dan genap\n");
-} else if ((x < 0) && (x % 2 == 1)) {
-printf("negatif dan ganjil\n");
-} else if ((x < 0) && (x % 2 == 0)) {
+#include <stdio.h>
+
+int main() {
+    int x;
+
+    // Misalnya kita mendapatkan input dari pengguna
+    printf("Masukkan nilai x: ");
+    scanf("%d", &x);
+
+    if ((x > 0) && (x % 2 == 1)) {
+        printf("positif dan ganjil\n");
+    } else if ((x > 0) && (x % 2 == 0)) {
+        printf("positif dan genap\n");
+    } else if ((x < 0) && (x % 2 == 1)) {
+        printf("negatif dan ganjil\n");
+    } else if ((x < 0) && (x % 2 == 0)) {
+        printf("negatif dan genap\n");
+    } else {
+        printf("x adalah nol\n");
+    }
+
+    return 0;
+}
