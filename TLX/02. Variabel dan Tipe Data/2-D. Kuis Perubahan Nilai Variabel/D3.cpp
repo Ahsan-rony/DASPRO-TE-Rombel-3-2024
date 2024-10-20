64c55c38a02a832044bd7f35618db3401d317e08
ridwannbg <ridwanbagus2806@gmail.com>
2024-10-20 10:09:51 +0700

    Update D3.cpp

--- TLX/02. Variabel dan Tipe Data/2-D. Kuis Perubahan Nilai Variabel/D3.cpp
+++ TLX/02. Variabel dan Tipe Data/2-D. Kuis Perubahan Nilai Variabel/D3.cpp
@@ -1,10 +1,16 @@
-#include <iostream>
+/Apakah keluaran dari program berikut?/
+ele#include <iostream>
 using namespace std;
 
 int main() {
     int x = 3;
     int y = 4;
+  // int merupakan singkatan dari integer, yaitu tipe data yang digunakan untuk merepresentasikan bilangan bulat (tanpa desimal) //
     x = y;
     y = x;
     cout << x << " " << y << endl;   
 }
+/*keluaran dari program tersebut adalah 4 4 karena x pada baris ke-10 bernilai 4 dan y bernilai 4,
+kemudian penjelasan lebih lanjut x pada baris ke-9 = y yaitu 4,kemudian y pada baris ke-10 y = x yaitu 4,
+karena pada baris ke-10 nilai x nya telah diperbarui menjadi bernilai 4, bukan 3 lagi 
+Maka hasil / (cout) variabel x y bernilai 4 4*/
