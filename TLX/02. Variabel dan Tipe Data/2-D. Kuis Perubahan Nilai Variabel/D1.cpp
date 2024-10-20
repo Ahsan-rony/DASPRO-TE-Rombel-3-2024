2fdacd63abc09a75ca93e9c6ecda06c0c34f1991
ridwannbg <ridwanbagus2806@gmail.com>
2024-10-20 10:08:37 +0700

    Update D1.cpp

--- TLX/02. Variabel dan Tipe Data/2-D. Kuis Perubahan Nilai Variabel/D1.cpp
+++ TLX/02. Variabel dan Tipe Data/2-D. Kuis Perubahan Nilai Variabel/D1.cpp
@@ -1,8 +1,13 @@
+/Apakah keluaran dari program berikut?/
 #include <iostream>
 using namespace std;
 
 int main() {
     int x = 3;
+  // int merupakan singkatan dari integer, yaitu tipe data yang digunakan untuk merepresentasikan bilangan bulat (tanpa desimal) //
     x = x + 1;
     cout << x << endl;   
 }
+/*keluaran dari program tersebut adalah 4 karena x bernilai 3,
+kemudian ditambahkan dengan +1.
+Maka hasil / (cout) variabel x bernilai 4*/
