1b95c25522c32eec4c60481faa6c7e1f8a7c3e4e
Rhasyaptr <rhasyasera@gmail.com>
2024-10-20 09:26:47 +0700

    Update fibonaci_rekursi.cpp

--- OSN/14. Rekursi Lanjut/fibonaci_rekursi.cpp
+++ OSN/14. Rekursi Lanjut/fibonaci_rekursi.cpp
@@ -7,3 +7,29 @@ int fibonacci(int N) {
         return fibonacci(N - 1) + fibonacci(N - 2);
     }
 }
+
+// Agar bahasa pemrograman itu bisa digunakan saya menambahkan fungsi main untuk dapat menginput angka (n)
+
+#include <cstdio>
+
+// Fungsi Fibonacci menggunakan rekursi
+int fibonacci(int N) {
+    if (N <= 1) {
+        return N;
+    } else {
+        return fibonacci(N - 1) + fibonacci(N - 2);
+    }
+}
+
+int main() {
+    int N;
+
+    // Meminta input dari user
+    printf("Masukkan angka N: ");
+    scanf("%d", &N);
+
+    // Menampilkan hasil dari fungsi fibonacci
+    printf("Fibonacci(%d) = %d\n", N, fibonacci(N));
+
+    return 0;
+}
