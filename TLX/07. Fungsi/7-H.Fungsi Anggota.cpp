bbd9a1b979248489d691307867cd261ffcdaedaa
Ignasius Adiwidya Aryanta <ignasiusra@gmail.com>
2024-10-20 08:38:15 +0700

    Update 7-H.Fungsi Anggota.cpp

--- TLX/07. Fungsi/7-H.Fungsi Anggota.cpp
+++ TLX/07. Fungsi/7-H.Fungsi Anggota.cpp
@@ -1,3 +1,10 @@
+/* Soal
+Pak Dengklek menemukan sebuah koran lama. Pada halaman pertama, terdapat artikel yang bertuliskan:
+lorem ipsum dolor sit amet platea sapien eu. primis suscipit dapibus tellus adipiscing dolor eu erat. sollicitudin ac eros accumsan duis ullamcorper hac odio imperdiet.
+Pak Dengklek meminta Anda untuk mencetak sebuah baris berisi seluruh karakter dari karakter ke-33 sampai dengan ke-40. Bantulah Pak Dengklek! Pak Dengklek memberikan petunjuk: Anda dapat memanfaatkan fungsi anggota substr().
+Perhatikan bahwa pada C++, indeks karakter pada string dimulai dari 0, sehingga karakter ke-33 ada pada indeks nomor 32.
+*/
+
 #include <iostream>
 #include <string>
 using namespace std;
